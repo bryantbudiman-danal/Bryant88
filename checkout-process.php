6b5d1a90c3c7b91579b84eb41e51f23447314baf
Bryant Budiman <bryant.budiman@danalcorp.com>
2018-07-02 13:31:12 -0700

    adding checkout-process.php

--- checkout-process.php
+++ checkout-process.php
@@ -1,6 +1,15 @@
 <?php
 	session_start();
 
+	function randString() {
+    	$char = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ012345678987654321QWERTYUIOPASDFGHJKLZXCVBNMmnbvcxzqwertyuioplkjhgfdsa";
+    	$char = str_shuffle($char);
+    	for($i = 0, $rand = '', $l = strlen($char) - 1; $i < 50; $i ++) {
+        	$rand .= $char{mt_rand(0, $l)};
+    	}
+    	return $rand;
+    }
+
 	$firstName = $_POST['firstName'];
 	$lastName = $_POST['lastName'];
 	$email = $_POST['email'];
@@ -11,40 +20,48 @@
 	$zip = $_POST['zip'];	
 	$country = $_POST['country'];
 
+	$identityArray = array("consumerFirstName" => "" . $firstName . "",
+						   "consumerLastName" => "" . $lastName . "",
+						   "consumerAddress1" => "" . $address1 . "",
+						   "consumerAddress2" => "" . $address2 . "",
+						   "consumerCity" => "" . $city . "",
+						   "consumerState" => "" . $state . "",
+						   "consumerPostalCode" => "" . $zip . "",
+						   "consumerEmailAddress" => "" . $email . ""
+				 	);
 
-	$data = array("name" => "Hagrid", "age" => "36");                                                                    
-	$data_string = json_encode($data);                                                                                   
-	                                                                                                                     
+	//$identityJSON = json_encode($identityArray);
+
+	$randomID = randString();
+
+	$parameters = array("merchantId" => "0218000710B56C", 
+				  "attributeGroups" => "matchScores", 
+				  "correlationId" => $randomID, 
+				  "intendedUseCase" => "RM",
+				  "identity" => "",
+				);
+
+	$parameters["identity"] = json_decode($identityArray, true); 
+
+	$parametersJSON = json_encode($parameters, JSON_PRETTY_PRINT);
+
+	$date = date("c");
+                                                           
 	$ch = curl_init('https://api-sbox.dnlsrv.com/cigateway/id/v1/matchAndAttributes');             
 	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);                                                                  
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $parametersJSON);                                                                  
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
-	curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array(  
+		'Authorization: qNl25zFXkJgsGR8vlhk57BelKaZPS20K',
+		'Accept: application/json',
+		'RequestTime: ' . $date,
 	    'Content-Type: application/json',                                                                                
-	    'Content-Length: ' . strlen($data_string))                                                                       
-	);                                                                                                                   
+	    'Content-Length: ' . strlen($parametersJSON))                                                                       
+	);                     
+
+	echo $parametersJSON;                                                                                              
 	                                                                                                                     
-	$result = curl_exec($ch);
+	//$result = curl_exec($ch);
 
 
-	// { 
-	// 	"merchantId":"00DF00000015", 
-	// 	"subMerchantId":"00DF00000016", 
-	// 	"consentID":"uyciydfoudqwg912863017", 
-	// 	"consentTimeStamp":"20130220142059", 
-	// 	"correlationId":"ABC0881973286793", 
-	// 	"authenticationKey":"abc0123456789", 
-	// 	"attributeGroups":"matchScores,serviceInfo,accountInfo,additionAccountInfo,deviceInfo,tok enInfo,changeInfo", 
-	// 	"intendedUseCase": "RM", 
-	// 	"identity": { 
-	// 	        "consumerFirstName":"Jane", 
-	// 	        "consumerLastName":"Smith", 
-	// 	        "consumerAddress1":"321 Main Street", 
-	// 	        "consumerAddress2":"Suite 22", 
-	// 	        "consumerCity":"New York", 
-	// 	        "consumerState":"NY", 
-	// 	        "consumerPostalCode":"10021", 
-	// 	        "consumerEmailAddress":jane.smith@email.com 
-	// 	} 
-	// } 
 ?>
