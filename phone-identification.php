1f939839d30e371aab0b557b22b1d78f280c03c2
Bryant Budiman <bryant.budiman@danalcorp.com>
2018-07-06 16:01:53 -0700

    testing phone id

--- phone-identification.php
+++ phone-identification.php
@@ -11,24 +11,26 @@
 	function generateEVURL() {
 		$url =  "http://mi-sbox.dnlsrv.com/msbox/id/kJlSiWWo";
 
+		echo "url is: " . $url . "\n\n\n";
+
+		$payload = "correlationid=" . randString() . "&timestamp=" . date("YmdHis") .
+			"&nonce=" . rand(pow(10, 4), pow(10, 4)-1);
+
+		echo "payload is: " . $payload . "\n\n\n";
+
 		$aesKey = "ExNYKNKh2iCwPGijJdP64A==";
-
-		$payload = "correlationid=" . randString() . "&TimeStamp=" . date("YmdHis") .
-			"&Nonce=" . rand(pow(10, 4), pow(10, 4)-1);
-
 	    // Remove the base64 encoding from our key
 	    $aesKey = base64_decode($aesKey);
 
 	    $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length('aes-128-ctr'));
 
-	    // Encrypt the data using AES 256 encryption in CBC mode using our encryption key and initialization vector.
 	    $encryptedPayload = openssl_encrypt($payload, 'aes-128-ctr', $aesKey, OPENSSL_RAW_DATA|OPENSSL_ZERO_PADDING, $iv);
 
 	    $encryptedPayload = base64_encode($encryptedPayload);
 
 	    $EVURL = $url . "?data=" . rawurlencode($encryptedPayload) . "&cipherSalt=" . rawurlencode($iv);
 
-	    return $EVURL;
+	    //return $EVURL;
 	}
 
 	echo generateEVURL();
