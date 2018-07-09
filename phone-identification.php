d432bd0ac218506be0b46e59b0ceea9c2efa189e
Bryant Budiman <bryant.budiman@danalcorp.com>
2018-07-09 12:17:49 -0700

    plz workity work

--- phone-identification.php
+++ phone-identification.php
@@ -2,27 +2,23 @@
 	function randString() {
     	$char = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ012345678987654321QWERTYUIOPASDFGHJKLZXCVBNMmnbvcxzqwertyuioplkjhgfdsa";
     	$char = str_shuffle($char);
-    	for($i = 0, $rand = '', $l = strlen($char) - 1; $i < 11$i ++) {
+    	for($i = 0, $rand = '', $l = strlen($char) - 1; $i < 38; $i ++) {
         	$rand .= $char{mt_rand(0, $l)};
     	}
     	return $rand;
     }
 
 	function generateRequestBody() {
-		$payload = 'correlationid=bryantbudiman'.
+		$payload = "correlationid=88888888" . 
 				   '&amp;timestamp=' . date("YmdHis") .
-				   '&amp;nonce=' . rand(10000,99999);
-
-				   echo $payload . "             ";
-
+				   '&amp;nonce='.rand(10000,99999);
 
 	    // Remove the base64 encoding from our key
 	    $aesKey = base64_decode("ExNYKNKh2iCwPGijJdP64A==");
 
  	    // Generate the cipher salt
-	    // $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length('aes-128-ctr'));
-	    // $iv = bin2hex($iv);
-	    $iv = randString();
+	    $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length('aes-128-ctr'));
+	    $iv = bin2hex($iv);
 
 	    $encryptedPayload = openssl_encrypt($payload, 'aes-128-ctr', $aesKey, OPENSSL_RAW_DATA|OPENSSL_ZERO_PADDING, $iv);
 
@@ -32,9 +28,9 @@
 
 		$iv = urlencode($iv);
 
-	    $requestBody = "data=" . $encryptedPayload . "&amp;cipherSalt=" . $iv;
+	    $requestBody = "cipherSalt=" . $iv . "&amp;data=" . $encryptedPayload;
 
-		
+		///
 	 //    $encryptedPayload = urldecode($encryptedPayload);
 	 //    $iv = urldecode($iv);
 
@@ -43,16 +39,18 @@
 		// $pleaseDecode = openssl_decrypt($decodedPayload, 'aes-128-ctr', $aesKey, OPENSSL_RAW_DATA|OPENSSL_ZERO_PADDING, $iv);
 
 		// echo "decodedPayload: " . $pleaseDecode;
-		
+		///
 
 		return $requestBody;
 	}
 
-    $postBody = generateRequestBody();
-	$url = 'http://mi-sbox.dnlsrv.com/msbox/id/kJlSiWWo?' . $postBody;
+	// $ch = curl_init('http://mi-sbox.dnlsrv.com/msbox/id/kJlSiWWo');          
 
-	echo $url;
-	// $ch = curl_init($url);      
+	$postBody = generateRequestBody();
+
+	// curl_setopt($ch, CURLOPT_POST, true);      
+
+	// curl_setopt($ch, CURLOPT_POSTFIELDS, $postBody);
 
 	// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);  
 
@@ -61,8 +59,11 @@
  //    curl_setopt($ch, CURLOPT_HTTPHEADER, array(  
  //    	'Authorization: qNl25zFXkJgsGR8vlhk57BelKaZPS20K',
  //        'Accept: application/json',
- //        'RequestTime: ' . $date                            
- //    ))                          
+ //        'RequestTime: ' . $date,
+	// 	'Content-Length: ' . strlen($postBody))                                
+ //    );                          
 
-	//esult = curl_exec($ch);
+	// $result = curl_exec($ch);
+
+	echo 'http://mi-sbox.dnlsrv.com/msbox/id/kJlSiWWo?' . $postBody;
 ?>
