1a38daf33047860ad4afe9ba210b3efd1b67bd8f
Bryant Budiman <bryant.budiman@danalcorp.com>
2018-07-09 15:42:37 -0700

    please work

--- getPhoneIdResult.php
+++ getPhoneIdResult.php
@@ -11,37 +11,13 @@
     return $rand;
   }
 
-  // START OF EVURL GENERATION //
-  $correlationId = randString(11);
-
-  date_default_timezone_set('UTC');
- 
-  $payload = "correlationid=" . $correlationid .
-             '&timestamp=' . date("YmdHis") .
-             '&nonce='.rand(10000,99999);
-
-  $aesKey = base64_decode("ExNYKNKh2iCwPGijJdP64A==");
-
-  $iv = randString(14);
-
-  $encryptedPayload = openssl_encrypt($payload, 'aes-128-ctr', $aesKey, OPENSSL_RAW_DATA|OPENSSL_ZERO_PADDING, $iv);
-
-  $encryptedPayload = base64_encode($encryptedPayload);
-
-  $encryptedPayload = urlencode($encryptedPayload);
-
-  $iv = urlencode($iv);
-
-  $requestBody = "data=" . $encryptedPayload . "&cipherSalt=" . $iv;
-
-  $EVURL = 'http://mi-sbox.dnlsrv.com/msbox/id/kJlSiWWo?' . $requestBody;
-  // END OF EVURL GENERATION //
+  $associationKey = $_GET['id']);
 
   $evurlRequest = curl_init($EVURL);
 
   $parameters = array("merchantId" => "0218000710B56C", 
                       "correlationId" => $correlationId,
-                      "associationKey" => "88rising"
+                      "associationKey" => $associationKey
                 );
 
   $parametersJSON = json_encode($parameters, JSON_PRETTY_PRINT);
@@ -70,16 +46,16 @@
   
   $result = json_decode($resultJSON, true);
 
-  $aesKey = base64_decode("BbRDqr+rvcdHsb63w49xJA==");
+  // $aesKey = base64_decode("BbRDqr+rvcdHsb63w49xJA==");
 
-  $iv =  trim($result['results']['cipherSalt']);
+  // $iv =  trim($result['results']['cipherSalt']);
 
-  $encryptedPayload = trim($result['results']['encryptedData']);
+  // $encryptedPayload = trim($result['results']['encryptedData']);
 
-  $decodedPayload = base64_decode($encryptedPayload);
+  // $decodedPayload = base64_decode($encryptedPayload);
 
-  $pleaseDecode = openssl_decrypt($decodedPayload, 'aes-128-ctr', $aesKey, OPENSSL_RAW_DATA|OPENSSL_ZERO_PADDING, $iv);
+  // $pleaseDecode = openssl_decrypt($decodedPayload, 'aes-128-ctr', $aesKey, OPENSSL_RAW_DATA|OPENSSL_ZERO_PADDING, $iv);
 
-  echo $pleaseDecode;
+  // echo $pleaseDecode;
 
 ?>
--- phone-identification.php
+++ phone-identification.php
@@ -8,48 +8,30 @@
     	return $rand;
     }
 
-	function generateRequestBody() {
-		$correlationid = randString(11);
+	$correlationId = randString(11);
 
-		date_default_timezone_set('UTC');
+	date_default_timezone_set('UTC');
  
-		$payload = 'correlationid=' . $correlationid .
-				   '&timestamp=' . date("YmdHis") .
-				   '&nonce=' . rand(10000,99999);
+	$payload = 'correlationid=' . $correlationId .
+			   '&timestamp=' . date("YmdHis") .
+			   '&nonce=' . rand(10000,99999);
 
-	    // Remove the base64 encoding from our key
-	    $aesKey = base64_decode("ExNYKNKh2iCwPGijJdP64A==");
+	// Remove the base64 encoding from our key
+	$aesKey = base64_decode("ExNYKNKh2iCwPGijJdP64A==");
 
-	    $iv = randString(14);
+	$iv = randString(14);
 
-	    $encryptedPayload = openssl_encrypt($payload, 'aes-128-ctr', $aesKey, OPENSSL_RAW_DATA|OPENSSL_ZERO_PADDING, $iv);
+	$encryptedPayload = openssl_encrypt($payload, 'aes-128-ctr', $aesKey, OPENSSL_RAW_DATA|OPENSSL_ZERO_PADDING, $iv);
 
-	    $encryptedPayload = base64_encode($encryptedPayload);
+	$encryptedPayload = base64_encode($encryptedPayload);
 
-	    $encryptedPayload = urlencode($encryptedPayload);
+	$encryptedPayload = urlencode($encryptedPayload);
 
-		$iv = urlencode($iv);
+    $iv = urlencode($iv);
 
-	    $requestBody = '&redirect=https://bryant88.azurewebsites.net/phone-identification-success.php' . '&data=' . $encryptedPayload . '&cipherSalt=' . $iv;
+	$requestBody = '&redirect=https://bryant88.azurewebsites.net/getPhoneIdResult.php?id=' . $correlationId . '&data=' . $encryptedPayload . '&cipherSalt=' . $iv;
 
-
-		///
-	 //    $encryptedPayload = urldecode($encryptedPayload);
-	 //    $iv = urldecode($iv);
-
-		// $decodedPayload = base64_decode($encryptedPayload);
-
-		// $pleaseDecode = openssl_decrypt($decodedPayload, 'aes-128-ctr', $aesKey, OPENSSL_RAW_DATA|OPENSSL_ZERO_PADDING, $iv);
-
-		// echo "decodedPayload: " . $pleaseDecode;
-		///
-
-		return $requestBody;
-	}          
-
-	$postBody = generateRequestBody();
-
-	$EVURL = 'http://mi-sbox.dnlsrv.com/msbox/id/kJlSiWWo?' . $postBody;
+	$EVURL = 'http://mi-sbox.dnlsrv.com/msbox/id/kJlSiWWo?' . $requestBody;
 
 	header('Location: ' . $EVURL);
 ?>
