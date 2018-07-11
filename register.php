1c47f9007ffe59e837674b8b1e70b6d6af0ee610
Bryant Budiman <bryant.budiman@danalcorp.com>
2018-07-11 13:07:22 -0700

    hehe

--- register.php
+++ register.php
@@ -56,7 +56,7 @@
 
   $pleaseDecode = openssl_decrypt($decodedPayload, 'aes-128-ctr', $aesKey, OPENSSL_RAW_DATA|OPENSSL_ZERO_PADDING, $iv);
 // echo $pleaseDecode;
-  $consumerInfo = json_encode($pleaseDecode);
+  $consumerInfo = json_decode($pleaseDecode, true);
   // $consumerInfo = $pleaseDecode['consumerInfo'];
   echo $consumerInfo["consumerInfo"];
 
