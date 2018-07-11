c41527abcdbd0fd1828852937bcd2208ec32132e
Bryant Budiman <bryant.budiman@danalcorp.com>
2018-07-11 13:05:05 -0700

    salak

--- register.php
+++ register.php
@@ -58,7 +58,7 @@
 // echo $pleaseDecode;
   $consumerInfo = json_encode($pleaseDecode);
   // $consumerInfo = $pleaseDecode['consumerInfo'];
-  echo $consumerInfo[0];
+  echo $consumerInfo["consumerInfo"];
 
   $firstName = $consumerInfo["consumerFirstName"];
   $lastName = $consumerInfo["consumerLastName"];
