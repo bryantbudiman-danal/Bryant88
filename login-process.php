4744f743e830a97b21b70c9f71337d03c23b231e
Bryant Budiman <bryant.budiman@danalcorp.com>
2018-07-12 10:35:37 -0700

    salak

--- login-process.php
+++ login-process.php
@@ -92,9 +92,9 @@
 
 	$response = curl_exec($ch);	
 
-	if($loginSuccess == true) {
-		header('Location: ../index.php'); 
-	} else {
-		header('Location: ../login.php?fail=true'); 
-	}
+	// if($loginSuccess == true) {
+	// 	header('Location: ../index.php'); 
+	// } else {
+	// 	header('Location: ../login.php?fail=true'); 
+	// }
 ?>
