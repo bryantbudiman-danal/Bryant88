b0bdf4515a9a378abe5e10d1791afee9ace8f944
Bryant Budiman <bryant.budiman@danalcorp.com>
2018-07-17 10:02:38 -0700

    adding excel stuff

---
+++ SiftScience/janekusuma.php
@@ -0,0 +1,131 @@
+<?php
+	$startDateRandom = 1483232461;
+	$endDateRandom = 1531761875;
+	$time = mt_rand($startDateRandom, $endDateRandom);
+
+	for ($x = 0; $x <= 10; $x++) {
+		////////////////////////////////////ADD TO CART//////////////////////////////////////////////
+		$ch = curl_init('https://api.siftscience.com/v205/events?return_score=true');
+
+		$id = mt_rand(1,3);
+		$id = (string)$id;
+		$quantity = mt_rand(1,10);
+
+		$price = 8; 
+		if($id == '2') {
+			$price = 88;
+		} else if($id == '3') {
+			$price = 888;
+		}
+
+		$itemInfo = array(
+							'$product_title' => $id, 
+							'$price' => (int)$price,
+							'$currency_code' => 'USD',
+							'$quantity' => (int)$quantity,
+							'$item_id' => $id,
+						);
+
+		$itemInfoJSON = json_encode($itemInfo);
+
+		$data = array(
+						'$type' => '$add_item_to_cart',
+						'$api_key' => 'd5e30e6affe617f1'
+						'$time' => $time,
+						'$user_id' => "jane.kusuma",
+					);
+
+		$data['$item'] = json_decode($itemInfoJSON, true);
+
+		$data_string = json_encode($data, JSON_PRETTY_PRINT);
+
+		curl_setopt($ch, CURLOPT_POST, true);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);      
+		curl_setopt($ch, CURLOPT_HEADER, array(
+			'Content-Type: application/json', 
+			'Content-Length: ' . strlen($data_string))
+		);
+
+		$response = curl_exec($ch);
+		////////////////////////////////////ADD TO CART END//////////////////////////////////////////////
+
+		/////////////////////////////////////CREATE_ORDER////////////////////////////////////////////////
+		$ch = curl_init('https://api.siftscience.com/v205/events');
+
+		$itemInfo = array(
+							'$product_title' => $id, 
+							'$price' => (int)$price,
+							'$currency_code' => 'USD',
+							'$quantity' => (int)$quantity,
+							'$item_id' => $id,
+						);
+
+		$itemInfoJSON = json_encode($itemInfo);
+
+		$time = $time + 200; 
+
+		$data = array(
+						'$type' => '$create_order',
+						'$api_key' => '3203af73a23bcb46',
+						'$amount' => 88888888,
+						'$currency_code' => 'USD',
+						'$time' => $time,
+						'$user_id' => "jane.kusuma",
+					);
+
+		$data_string = json_encode($data, JSON_PRETTY_PRINT);
+
+		curl_setopt($ch, CURLOPT_POST, true);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);   
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);      
+		curl_setopt($ch, CURLOPT_HEADER, array(
+			'Content-Type: application/json', 
+			'Content-Length: ' . strlen($data_string))
+		);
+
+		$response = curl_exec($ch);
+		////////////////////////////////////CREATE-ORDER END/////////////////////////////////////////////
+
+		////////////////////////////////////TRANSACTION//////////////////////////////////////////////////
+		$ch = curl_init('https://api.siftscience.com/v205/events');
+
+		$billing_address = array(
+							'$name' => "Jane Kusuma", 
+							'$phone' => "1-917-257-0947",
+							'$address_1' => "398 Raccoon Run",
+							'$city' => "Seattle",
+							'$region' => "Washington",
+							'$country' => "US",
+							'$zipcode' => "98109"
+						);
+
+		$billing_address = json_encode($billing_address);
+
+		$data = array(
+						'$type' => '$transaction',
+						'$api_key' => '3203af73a23bcb46',
+						'$amount' => 88888888,
+						'$currency_code' => 'USD',
+						'$time' => $time,
+						'$user_id' => "jane.kusuma",
+					);
+
+		$data['$billing_address'] = json_decode($billing_address, true);
+
+		$data_string = json_encode($data, JSON_PRETTY_PRINT);
+
+		curl_setopt($ch, CURLOPT_POST, true);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);   
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);      
+		curl_setopt($ch, CURLOPT_HEADER, array(
+			'Content-Type: application/json', 
+			'Content-Length: ' . strlen($data_string))
+		);
+
+		$response = curl_exec($ch);
+	 ///////////////////////////////////ADD TO CART END//////////////////////////////////////////////
+	}
+?>
--- index.php
+++ index.php
@@ -1,3 +1,19 @@
+<?php
+  require 'vendor/autoload.php';
+
+  use PhpOffice\PhpSpreadsheet\Spreadsheet;
+  use PhpOffice\PhpSpreadsheet\Writer\Xls;
+
+  $spreadsheet = new Spreadsheet();
+  $sheet = $spreadsheet->getActiveSheet();
+  $sheet->setCellValue('A1', 'Hello World !');
+
+  $writer = new Xlsx($spreadsheet);
+  $writer->save('hello world.xls');
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
