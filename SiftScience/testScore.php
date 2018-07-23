08138e44f499e51415b2ef4ef686e7ca1f79702f
Bryant Budiman <bryant.budiman@danalcorp.com>
2018-07-23 14:23:03 -0700

    hi

--- SiftScience/testScore.php
+++ SiftScience/testScore.php
@@ -1,5 +1,5 @@
 <?php
-	$ch = curl_init('https://api.siftscience.com/v205/score/verda.wehner/?api_key=e7e2cfa100771efb');
+	$ch = curl_init('https://api.siftscience.com/v205/score/tracey.hagenes/?api_key=e7e2cfa100771efb');
 	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 	curl_exec($ch); 
 ?>
