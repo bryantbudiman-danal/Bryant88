31de8c7304e26d8378a84333db9f18207e73fc59
Bryant Budiman <bryant.budiman@danalcorp.com>
2018-08-01 11:15:52 -0700

    hmm

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
                 if (mysqli_connect_errno($mysqli)) {
                   echo 'Failed to connect to MySQL: ' . mysqli_connect_error();
                 } else {
-                  if (($handle = fopen("badPeople3.csv", "r")) !== FALSE) {
+                  if (($handle = fopen("badPeople4.csv", "r")) !== FALSE) {
                     while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
 
                       $username = urlencode($data[0]);
@@ -25,7 +25,7 @@
 
                       $paymentAbuseScore = trim($result["scores"]["payment_abuse"]["score"]);
 
-                      $sql = "INSERT INTO people.badpeople3scorewithdecisions (username, accountAbuseScore,
+                      $sql = "INSERT INTO people.badpeople4scorewithdecisions (username, accountAbuseScore,
                       accountTakeoverScore, paymentAbuseScore)
                         VALUES ('" . $username  . "', '" . 
                         $accountAbuseScore . "', '"  . 
