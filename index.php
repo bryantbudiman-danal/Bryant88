5f7786a9e2a345278d12daef449a814cc2950472
Bryant Budiman <bryant.budiman@danalcorp.com>
2018-08-03 11:07:20 -0700

    hmm

--- index.php
+++ index.php
@@ -54,9 +54,11 @@
 
   <body id="page-top">
     <script type="text/javascript">
-      session_start();
+      <?php
+        session_start();
 
-      var _user_id = $_SESSION['user']; // Set to the user's ID, username, or email address, or '' if not yet known.
+        echo "var _user_id = " . $_SESSION['user'] , ";"; // Set to the user's ID, username, or email address, or '' if not yet known.
+      ?>
       var _session_id = 'unique_session_id';
       var _api_key = '88888888';
       var _timestamp = Math.round(Date.now()/1000);
