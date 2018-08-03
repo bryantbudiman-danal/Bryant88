a5cf2afaaace9062f259826088dcfb7a52e6a53e
Bryant Budiman <bryant.budiman@danalcorp.com>
2018-08-03 02:00:30 -0700

    hmm

--- jsSnippetToDatabase.php
+++ jsSnippetToDatabase.php
@@ -1,9 +1,15 @@
 <?php
 	session_start();
 
-	echo $_SERVER['REMOTE_ADDR'];
-
 	echo $_GET['url'];
 
+	$ipStackURL = 'http://api.ipstack.com/' . $_SERVER['REMOTE_ADDR'] . '?access_key=46060a72ff525ad6e6f6a8998eefa90d';
 
+	$ch = curl_init('https://api.siftscience.com/v205/events?return_score=true');
+
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);   
+
+	$response = curl_exec($ch);
+	
+	echo $response;  
 ?>
