626c55a7ed04075a39755198cb0f1792d9cf2b45
Bryant Budiman <bryant.budiman@danalcorp.com>
2018-08-07 10:49:28 -0700

    hmm:

--- maxMind.php
+++ maxMind.php
@@ -1,17 +1,14 @@
 <?php
 	$userID = "135014";
 	$password = "GUp1giLKX7wN";
-	$basicAuth =  utf8_encode($userID . ":" . $password);
-	$basicAuth = base64_encode($basicAuth);
 
 	$ch = curl_init('https://geoip.maxmind.com/geoip/v2.1/city/172.58.110.165');
 
 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);   
-	curl_setopt($ch, CURLOPT_HEADER, array(
-		'Authorization: Basic ' . $basicAuth)
-	);
+	curl_setopt($ch, CURLOPT_HTTPAUTH, "CURLAUTH_BASIC");
+	curl_setopt($ch, CURLOPT_USERPWD, $userID . ":" . $password);
 
 	$response = curl_exec($ch);
 
