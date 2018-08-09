04a13431fe586429a68078b8a1fc2a9ec626d29f
Bryant Budiman <bryant.budiman@danalcorp.com>
2018-08-09 13:14:50 -0700

    ga

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
       })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
       ga('create', 'UA-123580463-1', 'auto');
-      ga('send');
+      ga('send', 'pageview');
     </script>
     <!-- End Google Analytics -->
 
