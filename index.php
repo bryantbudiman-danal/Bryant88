932cc052eccb8120c009b29094e6e85702363904
Bryant Budiman <bryant.budiman@danalcorp.com>
2018-08-10 10:08:22 -0700

    js stuff

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 
     <!-- Google Analytics -->
     <script>
-      var Q = window.Q = (new Date).getTime()
+      var Q = window.Q = (new Date).getTime();
 
       (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
       (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
