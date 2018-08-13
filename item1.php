367a20e57bef7216fe1e7b31b4f535e55bbb29b5
Bryant Budiman <bryant.budiman@danalcorp.com>
2018-08-13 00:48:28 -0700

    hehe

--- item1.php
+++ item1.php
@@ -11,6 +11,19 @@
 
     <title>Bryant88 Cool Sneakers</title>
 
+    <!-- JS Snippet -->
+    <script>
+      var Q = window.Q = (new Date).getTime();
+
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');    
+    </script>
+    <script src="Bryant88.js"></script>
+    <!-- End JS Snippet -->
+
+
     <!-- Bootstrap core CSS -->
     <link href="https://blackrockdigital.github.io/startbootstrap-agency/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
--- register.php
+++ register.php
@@ -329,6 +329,18 @@
 
     <title>Bryant88</title>
 
+    <!-- JS Snippet -->
+    <script>
+      var Q = window.Q = (new Date).getTime();
+
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');    
+    </script>
+    <script src="Bryant88.js"></script>
+    <!-- End JS Snippet -->    
+
     <!-- Bootstrap core CSS -->
     <link href="https://getbootstrap.com/docs/4.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
