2e875bc532da78eec604ed9d8eab84226137d859
Bryant Budiman <bryant.budiman@danalcorp.com>
2018-08-13 12:02:31 -0700

    haha

--- cart.php
+++ cart.php
@@ -10,16 +10,24 @@
 
         <!-- JS Snippet -->
         <script>
-          var Q = window.Q = (new Date).getTime();
+          <?php
+            session_start();
 
+            $username = "-88";
+
+            if(isset($_SESSION['user'])) $username = $_SESSION['user'];
+          ?>
+
+          var username = window.username = "<?php echo $username; ?>";
+          console.log(username);
           (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
           (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
           m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');  
+          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-          ga('create', 'UA-123580463-1', 'auto');  
+          ga('create', 'UA-123580463-1', 'auto');    
         </script>
-        <script src="Bryant88.js"></script>
+        <script src="snippet88.js"></script>
         <!-- End JS Snippet -->
             
         <!-- Bootstrap core CSS -->
